589fc81fb28a2718e0b858b70cdd378a6b5a1bb2
healtrex <christian.steckler@gmail.com>
2019-07-11 10:15:54 -0400

    first commit

---
+++ src/siteMetadata.js
@@ -0,0 +1,11 @@
+export default {
+  // The blog's title as it appears in the layout header, and in the document
+  // <title> tag.
+  title: "nomos.io",
+
+  author: "Shay Banerjee & Christian Steckler",
+  description: "Meditations on technology, society, and power.",
+
+  // The number of posts to a page on the site index.
+  indexPageSize: 5,
+}
